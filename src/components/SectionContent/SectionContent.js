5bc5c35ef5d44649cf130ea78df4a2c78829e004
Sanskar <sanskar.gupta@target.com>
2020-08-01 13:56:04 +0530

    addded adding of links

--- src/components/SectionContent/SectionContent.js
+++ src/components/SectionContent/SectionContent.js
@@ -1,24 +1,24 @@
-import React, {useEffect} from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import DetailCard from "../Common/DetailCard";
-import Button from "@material-ui/core/Button";
+import React, { useEffect } from 'react'
+import PropTypes from 'prop-types'
+import { makeStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
+import Typography from '@material-ui/core/Typography'
+import Box from '@material-ui/core/Box'
+import DetailCard from '../Common/DetailCard'
+import Button from '@material-ui/core/Button'
 import AddIcon from '@material-ui/icons/Add'
-import DialogTitle from "@material-ui/core/DialogTitle/DialogTitle";
-import DialogContent from "@material-ui/core/DialogContent/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
-import TextField from "@material-ui/core/TextField/TextField";
-import DialogActions from "@material-ui/core/DialogActions/DialogActions";
-import Dialog from "@material-ui/core/Dialog/Dialog";
-import {db} from "../../firebase";
+import DialogTitle from '@material-ui/core/DialogTitle/DialogTitle'
+import DialogContent from '@material-ui/core/DialogContent/DialogContent'
+import DialogContentText from '@material-ui/core/DialogContentText'
+import TextField from '@material-ui/core/TextField/TextField'
+import DialogActions from '@material-ui/core/DialogActions/DialogActions'
+import Dialog from '@material-ui/core/Dialog/Dialog'
+import { db } from '../../firebase'
 
 function TabPanel(props) {
-    const { children, value, index, ...other } = props;
+    const { children, value, index, ...other } = props
 
     return (
         <div
@@ -34,119 +34,121 @@ function TabPanel(props) {
                 </Box>
             )}
         </div>
-    );
+    )
 }
 
 TabPanel.propTypes = {
     children: PropTypes.node,
     index: PropTypes.any.isRequired,
     value: PropTypes.any.isRequired,
-};
-
-function a11yProps(index) {
-    return {
-        id: `nav-tab-${index}`,
-        'aria-controls': `nav-tabpanel-${index}`,
-    };
 }
-
 function LinkTab(props) {
     return (
         <Tab
             component="a"
             onClick={(event) => {
-                event.preventDefault();
+                event.preventDefault()
             }}
             {...props}
         />
-    );
+    )
 }
-
 
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
         backgroundColor: theme.palette.background.paper,
     },
-}));
+}))
 
-export default function SectionContent() {
-    const classes = useStyles();
-    const [value, setValue] = React.useState(0);
-    const [modalOpen, setModalOpen] = React.useState(false);
-    const [urlDetails, setUrlDetails]= React.useState({})
-    const [contentList, setContentList] = React.useState([])
+export default function SectionContent(props) {
+    const classes = useStyles()
+    const [value, setValue] = React.useState(0)
+    const [modalOpen, setModalOpen] = React.useState(false)
+    const [urlDetails, setUrlDetails] = React.useState({})
+    const [section, setSection] = React.useState({})
 
-
-    useEffect(()=>{
-        let folders=[]
+    useEffect(() => {
         db.collection('sections').onSnapshot((snapshot) => {
-           folders=snapshot.docs.map((doc) => doc.data())
-            const articleFolder = folders.filter(function (folder) {
-                return folder.sectionName === 'articles'
-            })[0]
-            setContentList(articleFolder.contents)
+            snapshot.docs.forEach((doc) => {
+                if (doc.data().sectionName === props.match.params.sectionName) {
+                    let obj = {}
+                    obj['id'] = doc.id
+                    obj['contents'] = doc.data().contents
+                    setSection(obj)
+                }
+            })
         })
+    }, [])
 
-    },[])
-
-
-    const handleAddLinkClick=()=>{
-            setModalOpen(true)
+    const handleAddLinkClick = () => {
+        setModalOpen(true)
     }
 
-    const handleChange=(event, name)=>{
-        let obj= Object. assign({}, urlDetails)
-        obj[name]=event.target.value
-       setUrlDetails(obj)
+    const handleChange = (event, name) => {
+        let obj = Object.assign({}, urlDetails)
+        obj[name] = event.target.value
+        obj['alreadyExist'] = true
+        setUrlDetails(obj)
     }
 
     const handleChangeOfTabs = (event, newValue) => {
-        setValue(newValue);
-    };
-
-    const handleLinkSave=()=>{
-        const object = {
-            url: urlDetails.url,
-            title: urlDetails.title,
-            alreadyExist: true
-
-        }
-
+        setValue(newValue)
     }
 
+    const handleLinkSave = () => {
+        const newContentArray = section.contents ? section.contents : []
+        newContentArray.push(urlDetails)
+        db.collection('sections')
+            .doc(section.id)
+            .update({
+                contents: newContentArray,
+            })
+            .then(() => {
+                setModalOpen(false)
+            })
+    }
 
     return (
         <div className={classes.root}>
-            <AppBar position="static" style={{backgroundColor: '#013220'}}>
+            <AppBar position="static" style={{ backgroundColor: '#013220' }}>
                 <Tabs
                     variant="fullWidth"
                     value={value}
                     onChange={handleChangeOfTabs}
                     aria-label="nav tabs example"
                 >
-                    <LinkTab label={<p style={{fontSize: '.8rem', fontWeight: 'bold'}}>publications</p>}  />
-                    <LinkTab label={<p style={{fontSize: '.8rem', fontWeight: 'bold'}}>articles</p>} />
-
+                    <LinkTab
+                        label={
+                            <p style={{ fontSize: '.8rem', fontWeight: 'bold' }}>
+                                publications
+                            </p>
+                        }
+                    />
+                    <LinkTab
+                        label={
+                            <p style={{ fontSize: '.8rem', fontWeight: 'bold' }}>articles</p>
+                        }
+                    />
                 </Tabs>
             </AppBar>
-            <TabPanel  value={value} index={0}>
+            <TabPanel value={value} index={0}>
                 <Button
                     variant="outlined"
                     color="default"
-                    startIcon={<AddIcon/>}
+                    startIcon={<AddIcon />}
                     onClick={handleAddLinkClick}
                 >
                     Create new link
                 </Button>
 
-                <div className='sectionContent_tab'>
-                    {contentList.map(content=>{
-                        return <DetailCard title={content.title} url={content.url}/>
+                <div className="sectionContent_tab">
+                    {section?.contents?.map((content) => {
+                        return <DetailCard title={content.title} url={content.url} />
                     })}
                 </div>
                 <Dialog open={modalOpen} maxWidth="xl">
-                    <DialogTitle id="form-dialog-title">Existing Article Details</DialogTitle>
+                    <DialogTitle id="form-dialog-title">Article Details</DialogTitle>
                     <DialogContent>
                         <DialogContentText>
                             Enter the title and url of the article
@@ -154,16 +156,18 @@ export default function SectionContent() {
                         <TextField
                             autoFocus
                             margin="dense"
-                            id="title"
+                            label="Title"
+                            variant="outlined"
                             fullWidth
-                            onChange={event => handleChange(event, 'title')}
+                            onChange={(event) => handleChange(event, 'title')}
                         />
                         <TextField
                             autoFocus
                             margin="dense"
-                            id="url"
+                            label="Url"
                             fullWidth
-                            onChange={event => handleChange(event, 'url')}
+                            variant="outlined"
+                            onChange={(event) => handleChange(event, 'url')}
                         />
                     </DialogContent>
                     <DialogActions>
@@ -179,7 +183,6 @@ export default function SectionContent() {
             <TabPanel value={value} index={1}>
                 Coming soon....
             </TabPanel>
-
         </div>
-    );
+    )
 }
